3e6247975339e007412ba087cf2c7be16281de44
Alex-Malyshenko <alis_mal@mail.ru>
2023-06-08 00:49:32 +0300

    End HomeWork Lesson3

---
+++ HomeWorkLesson3/Freelancer.java
@@ -0,0 +1,28 @@
+
+/**
+ * TODO: Спроектировать класс Freelancer самостоятельно в рамках домашнего задания.
+ *  *20*8
+ */
+public class Freelancer extends Employee {
+    private int hoursWorked;
+
+    public Freelancer(String name, String surname, double salary, int age, int hoursWorked) {
+        super(name, surname, salary, age);
+        this.hoursWorked = hoursWorked;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hoursWorked * salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Фрилансер; Возраст %d ; В текущем месяце заработная плата составила (почасовая оплата): %.2f (руб.)",
+                surname, name, age, calculateSalary());
+    }
+}
---
+++ HomeWorkLesson3/Worker.java
@@ -0,0 +1,18 @@
+
+public class Worker extends Employee {
+
+    public Worker(String name, String surname, double salary, int age) {
+        super(name, surname, salary, age);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Основной сотрудник; Возраст %d ; Среднемесячная заработная плата (фиксированная месячная оплата): %.2f (руб.)",
+                surname, name, age, calculateSalary());
+    }
+}
